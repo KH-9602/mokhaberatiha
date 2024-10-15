68e34c8e83c47293ef34a84b1c65a09cde90cb38
KH-9602 <kh1998xd@gmail.com>
2024-10-15 17:10:05 +0330

    first commit

--- js/index.js
+++ js/index.js
@@ -22,6 +22,29 @@ jQuery(document).ready(function(){
         }
     });
 
+
+    jQuery('[data-toggle="tooltip"]').tooltip();
+
+    jQuery('.hero').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:8,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
 });
 
 
